e54b102987896e730a782f607214ec71950ad3ca
umansky81 <umansky81@gmail.com>
2025-08-03 22:19:52 +0300

    Update Main.py

--- Main.py
+++ Main.py
@@ -21,8 +21,7 @@ coordinates_url = "http://api.openweathermap.org/geo/1.0/direct"
 
 # UI Setup
 st.set_page_config(page_title="Weather App", page_icon="🌤️", layout="centered")
-st.markdown(
-    "<h1 style='text-align: center; font-size: 36px;'>🌦️ Welcome to DS20 Weather Project!</h1>",unsafe_allow_html=True)
+st.markdown("<h1 style='text-align: center; font-size: 36px;'>🌦️ Welcome to DS20 Weather Project!</h1>",unsafe_allow_html=True)
 st.markdown("<h4 style='text-align: center; font-size: 20px;'>Enter a city name or select a location on the map to get weather updates.</h4>",unsafe_allow_html=True)
 
 # --- Input Options ---
